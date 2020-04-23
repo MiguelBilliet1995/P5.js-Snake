bc93c43d0e6ad59471f55b6f97132a27fd657421
MiguelBilliet1995 <miguel.billiet@student.ehb.be>
2020-04-23 14:43:48 +0200

    mainCommit

---
+++ sketch.js
@@ -0,0 +1,99 @@
+var s;
+var scl = 50;
+var f;
+var f_special;
+var f_special_activated = false;
+var framert = 5;
+
+function setup() {
+  createCanvas(1000,1000);
+  s = new Snake();
+  f = new Food();
+  f_special = new Food();
+  frameRate(framert);
+  pickLocation();
+}
+
+function requestSpecial(randm = true){
+  if(randm===true){
+    var kans = 3; // hoe lager nummer hoe meer kans op special food 
+    var rnd1 = ceil(random(0,kans));
+    var rnd2 = ceil(random(0,kans));
+    if(rnd1==rnd2){
+      f_special_activated = true;
+    }else{
+      f_special_activated = false;
+    }
+  }else{
+    f_special_activated = true;
+  }
+  
+  if(f_special_activated===true){
+    console.log("f_special activated");
+    var cols = floor(width/scl);
+    var rows = floor(height/scl);
+    foodLocation = createVector(floor(random(cols)),floor(random(rows)));
+    foodLocation.mult(scl);
+    f_special.initializeFood(foodLocation,true);
+  }
+}
+
+function pickLocation(){
+  var cols = floor(width/scl);
+  var rows = floor(height/scl);
+  foodLocation = createVector(floor(random(cols)),floor(random(rows)));
+  foodLocation.mult(scl);
+  f.initializeFood(foodLocation);
+}
+
+function draw() {
+  background(51);
+  s.death();
+  s.update();
+  s.show();
+  
+  if(f.eat()===true){
+    pickLocation();
+    requestSpecial();
+  }
+  
+  if(f_special.eat()===true){
+    f_special_activated = false;
+  }
+  
+  //fill(0);
+  text("score = " + s.score + "/" + s.targetScore , 1000 - 100 , 20);
+  text("level = " + s.level , 1000 - 100 , 40);
+  
+  var RGBcol = f.getColor();
+  var RGBarray = RGBcol.array();
+  fill(RGBarray[0],RGBarray[1],RGBarray[2]);
+  rect(f.x,f.y,scl,scl);
+  
+  if(f_special_activated===true){
+    RGBcol = f_special.getColor();
+    RGBarray = RGBcol.array();
+    fill(RGBarray[0],RGBarray[1],RGBarray[2]);
+    rect(f_special.x,f_special.y,scl,scl);
+  }
+}
+
+function keyPressed(){
+  if(keyCode == UP_ARROW){
+    if(s.yspeed !== 1){
+      s.dir(0 , -1);
+    }
+  }else if(keyCode == DOWN_ARROW){
+    if(s.yspeed !== -1){
+      s.dir(0 , 1);
+    }
+  }else if(keyCode == LEFT_ARROW){
+    if(s.xspeed !== 1){
+      s.dir(-1, 0);
+    }
+  }else if(keyCode == RIGHT_ARROW){
+    if(s.xspeed !== -1){
+      s.dir(1, 0);
+    }
+  }
+}
---
+++ snake.js
@@ -0,0 +1,83 @@
+function Snake(){
+  this.x = 0;
+  this.y = 0;
+  this.xspeed = 1;
+  this.yspeed = 0;
+  this.total = 0;
+  this.tail = [];
+  
+  this.score = 0;
+  this.targetScore = 5;
+  this.level = 1;
+  this.infisibl = false;
+  
+  this.snakeColor = [255,255,255]
+  
+  this.addScore = function(x){
+    for(one = 0; one < x; one++){
+        this.score ++;
+        // check if level up
+        if(this.score === this.targetScore ){
+          this.level ++;
+          this.targetScore = this.score * ((this.level + 1) ^ 0.1);
+          framert = framert + 1;
+          frameRate(framert);
+        }
+      }
+  }
+  
+  this.dir = function(x,y){
+    this.xspeed = x;
+    this.yspeed = y;
+  }
+  
+  this.death = function(){
+    for(var i=0;i<this.tail.length;i++){
+        var pos = this.tail[i];
+        var d = dist(this.x, this.y, pos.x, pos.y);
+        if(d<1){
+          this.total = 0;
+          this.tail = [];
+          this.score = 0;
+          this.level = 1;
+          framert = 5;
+          frameRate(framert);
+          this.targetScore = 5
+          this.snakeColor = [255,255,255];
+        }
+      }
+  }
+  
+  this.update = function(){
+    if(this.total == this.tail.length){
+      for(var i=0;i<this.tail.length-1;i++){
+        this.tail[i] = this.tail[i+1];
+      }
+    }
+    this.tail[this.total-1] = createVector(this.x,this.y);
+    
+    this.x = this.x + this.xspeed*scl;
+    this.y = this.y + this.yspeed*scl;
+    
+    this.x = constrain(this.x,0,width-scl);
+    this.y = constrain(this.y,0,height-scl);
+  }
+  
+  this.show = function(){
+    fill(this.snakeColor[0],this.snakeColor[1],this.snakeColor[2]);
+    for(var i=0;i<this.tail.length;i++){
+      rect(this.tail[i].x,this.tail[i].y,scl,scl);
+    }
+    rect(this.x,this.y,scl,scl);
+  }
+  
+  this.invsible = function(){
+    if(this.infisibl == false){
+      this.snakeColor = [58,58,58];
+      this.infisibl = true;
+    }else{
+      this.snakeColor = [255,255,255];
+      this.infisibl = false;
+    }
+  }
+}
