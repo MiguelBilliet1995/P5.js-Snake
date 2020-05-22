ee9e7074738b5d89fb60d2028bb83a3a034cb9d4
MiguelBilliet1995 <miguel.billiet@student.ehb.be>
2020-05-22 16:04:10 +0200

    node js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+var five = require('johnny-five'),
+  board = new five.Board();
+
 var s;
 var scl = 50;
 var f;
@@ -6,7 +9,7 @@ var f_special_activated = false;
 var framert = 5;
 
 function setup() {
-  createCanvas(1000,1000);
+  createCanvas(1000, 1000);
   s = new Snake();
   f = new Food();
   f_special = new Food();
@@ -14,34 +17,34 @@ function setup() {
   pickLocation();
 }
 
-function requestSpecial(randm = true){
-  if(randm===true){
+function requestSpecial(randm = true) {
+  if (randm === true) {
     var kans = 3; // hoe lager nummer hoe meer kans op special food 
-    var rnd1 = ceil(random(0,kans));
-    var rnd2 = ceil(random(0,kans));
-    if(rnd1==rnd2){
+    var rnd1 = ceil(random(0, kans));
+    var rnd2 = ceil(random(0, kans));
+    if (rnd1 == rnd2) {
       f_special_activated = true;
-    }else{
+    } else {
       f_special_activated = false;
     }
-  }else{
+  } else {
     f_special_activated = true;
   }
-  
-  if(f_special_activated===true){
+
+  if (f_special_activated === true) {
     console.log("f_special activated");
-    var cols = floor(width/scl);
-    var rows = floor(height/scl);
-    foodLocation = createVector(floor(random(cols)),floor(random(rows)));
+    var cols = floor(width / scl);
+    var rows = floor(height / scl);
+    foodLocation = createVector(floor(random(cols)), floor(random(rows)));
     foodLocation.mult(scl);
-    f_special.initializeFood(foodLocation,true);
+    f_special.initializeFood(foodLocation, true);
   }
 }
 
-function pickLocation(){
-  var cols = floor(width/scl);
-  var rows = floor(height/scl);
-  foodLocation = createVector(floor(random(cols)),floor(random(rows)));
+function pickLocation() {
+  var cols = floor(width / scl);
+  var rows = floor(height / scl);
+  foodLocation = createVector(floor(random(cols)), floor(random(rows)));
   foodLocation.mult(scl);
   f.initializeFood(foodLocation);
 }
@@ -51,49 +54,96 @@ function draw() {
   s.death();
   s.update();
   s.show();
-  
-  if(f.eat()===true){
+
+  if (f.eat() === true) {
     pickLocation();
     requestSpecial();
   }
-  
-  if(f_special.eat()===true){
+
+  if (f_special.eat() === true) {
     f_special_activated = false;
   }
-  
+
   //fill(0);
-  text("score = " + s.score + "/" + s.targetScore , 1000 - 100 , 20);
-  text("level = " + s.level , 1000 - 100 , 40);
-  
+  text("score = " + s.score + "/" + s.targetScore, 1000 - 100, 20);
+  text("level = " + s.level, 1000 - 100, 40);
+
   var RGBcol = f.getColor();
   var RGBarray = RGBcol.array();
-  fill(RGBarray[0],RGBarray[1],RGBarray[2]);
-  rect(f.x,f.y,scl,scl);
-  
-  if(f_special_activated===true){
+  fill(RGBarray[0], RGBarray[1], RGBarray[2]);
+  rect(f.x, f.y, scl, scl);
+
+  if (f_special_activated === true) {
     RGBcol = f_special.getColor();
     RGBarray = RGBcol.array();
-    fill(RGBarray[0],RGBarray[1],RGBarray[2]);
-    rect(f_special.x,f_special.y,scl,scl);
+    fill(RGBarray[0], RGBarray[1], RGBarray[2]);
+    rect(f_special.x, f_special.y, scl, scl);
   }
 }
 
-function keyPressed(){
-  if(keyCode == UP_ARROW){
-    if(s.yspeed !== 1){
-      s.dir(0 , -1);
+/*function keyPressed() {
+  if (keyCode == UP_ARROW) {
+    if (s.yspeed !== 1) {
+      s.dir(0, -1);
     }
-  }else if(keyCode == DOWN_ARROW){
-    if(s.yspeed !== -1){
-      s.dir(0 , 1);
+  } else if (keyCode == DOWN_ARROW) {
+    if (s.yspeed !== -1) {
+      s.dir(0, 1);
     }
-  }else if(keyCode == LEFT_ARROW){
-    if(s.xspeed !== 1){
+  } else if (keyCode == LEFT_ARROW) {
+    if (s.xspeed !== 1) {
       s.dir(-1, 0);
     }
-  }else if(keyCode == RIGHT_ARROW){
-    if(s.xspeed !== -1){
+  } else if (keyCode == RIGHT_ARROW) {
+    if (s.xspeed !== -1) {
       s.dir(1, 0);
     }
   }
-}
+}*/
+
+
+
+board.on('ready', function () {
+  console.log('OK');
+  this.pinMode(0, five.Pin.ANALOG); // potentiometer
+  this.pinMode(1, five.Pin.ANALOG); // photoresistor
+  this.pinMode(2, five.Pin.INPUT); // left
+  this.pinMode(4, five.Pin.INPUT); // down
+  this.pinMode(7, five.Pin.INPUT); // right
+  this.pinMode(8, five.Pin.INPUT); // up
+  this.pinMode(12, five.Pin.INPUT); // reset
+  this.pinMode(13, five.Pin.INPUT); // select
+
+  /*this.analogRead(0, function (voltage) {
+    framert = map(voltage, 0, 255, 1, 5);
+  });*/
+
+  this.digitalRead(8, function (voltage) {
+    if (s.yspeed !== 1) {
+      console.log("up");
+      s.dir(0, -1);
+    }
+  })
+
+  this.digitalRead(4, function (voltage) {
+    if (s.yspeed !== -1) {
+      console.log("down");
+      s.dir(0, 1);
+    }
+  })
+
+  this.digitalRead(2, function (voltage) {
+    if (s.xspeed !== 1) {
+      console.log("left");
+      s.dir(-1, 0);
+    }
+  })
+
+  this.digitalRead(7, function (voltage) {
+    if (s.xspeed !== -1) {
+      console.log("right");
+      s.dir(1, 0);
+    }
+  })
+
+});
