39451782183514126ba93ebac64ebc18ae672a87
MiguelBilliet1995 <miguel.billiet@student.ehb.be>
2020-05-22 19:59:42 +0200

    readme

---
+++ README.md
@@ -0,0 +1,10 @@
+Je heb nodeJS hiervoor nodig.
+
+sluit **eerst** uw Arduino aan.
+
+met deze commando start je het spel op:
+1. `cd nodeserver`
+2. `npm start`
+
+nadien open uw browser en ga naar
+`http://localhost:5000`
--- import_template.js
+++
@@ -1,9 +0,0 @@
-var five = require('johnny-five'),
-    board = new five.Board();
-
-board.on('ready', function () {
-    this.pinMode(1, five.Pin.ANALOG); // photoresistor
-    this.analogRead(1, function (voltage) {
-        console.log(voltage);
-    });
-});
